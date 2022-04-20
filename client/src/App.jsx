ddf183214ce9bd81ba45a42f5f3030eeeb969a9d
sarra-lejmi <sarra.lejmi2@gmail.com>
2022-04-20 02:21:43 +0100

    version1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { Chat} from 'stream-chat-react';
+import cookies from 'universal-cookie';
+
+import { ChannelContainer, ChannelListContainer } from './components';
+
+import './App.css'
+
+const apiKey = 'x9ujmgzvuevv';
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      <h1>DocChat App</h1>
+    <div className="app_wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer/>
+        <ChannelContainer/>
+      </Chat>
     </div>
   )
 }

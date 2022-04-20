5c8eb3500462b224589ab08f4105a873b1804c24
sarra-lejmi <sarra.lejmi2@gmail.com>
2022-04-21 00:10:25 +0100

    versionServer3

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,15 +1,39 @@
-const signup = () => {
-    try {
+const { connect} = require('getstream');
+const bcrypt = require('bcrypt');
+const StreamChat = require('stream-chat');
+const crypto = require('crypto');
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const api_id = process.env.STREAM_APP_ID;
 
-    }catch(error){
+const signup = async () => {
+    try {
+        const { fullName, username, password, phoneNumber } = req.body;
+        const userId = crypto.randomBytes(16).toString('hex');
+        const serverClient = connect(api_key, api_secret, app_id);
+        const hashedPassword = await bcrypt.hash(password, 10);// 10: specifies = the level of uncryption
+        const token = serverClient.createUserToken(userId);
+        res.status(200).json({token, fullName, username, userId, hashedPassword, phoneNumber});
+;    }catch(error){
         console.log(error);
         res(500).json({message: error});
     }
 };
 
-const login = (req, res) => {
+const login = async (req, res) => {
     try {
-
+        const { username, password} = req.body;
+        const serverClient = connect(api_key, api_secret, app_id);
+        const client = StreamChat.getInstance(api_key, api_secret); //query all the users from the database
+        const {users} = await client.queryUsers({name: username});
+        if(!users.length) return res.status(400).json({message: 'User not found'});
+        const success = await bcrypt.compare(password, users[0].hashedPassword);
+        const token = serverClient.createUserToken(users[0].id);
+        if(success){
+            res.status(200).json({token, fullName: users[0].fullName, username, userId: users[0].id});
+        } else{
+            res.status(500).json({message: 'Incorrect password'});
+        }
     }catch(error){
         console.log(error);
         res(500).json({message: error});
